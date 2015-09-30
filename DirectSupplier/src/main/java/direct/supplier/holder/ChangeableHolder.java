e37120a686daf7e05b96cdc676051b1cd38ee6bf
NawaMan <nawa@dssbsoft.com>
2015-09-30 00:14:41 -0600

    Add ChangeableHolder.

---
+++ DirectSupplier/src/main/java/direct/supplier/holder/ChangeableHolder.java
@@ -0,0 +1,102 @@
+package direct.supplier.holder;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import direct.supplier.SupplierSupplier;
+
+/**
+ * This resource holder holds a value that can be changed.
+ * 
+ * @author NawaMan
+ */
+public class ChangeableHolder<V>
+        implements SupplierSupplier<V> {
+    
+    /** The key to be used if no key is to be assigned. */
+    public final static Object NOKEY = new Object();
+    
+    private final AtomicReference<V> value = new AtomicReference<>();
+    
+    private final Optional<Object> key;
+    
+    /**
+     * Construct a holder with a key. If the key is {@code ChangeableHolder.NOKEY}, then no key is assigned.
+     * 
+     * @param  key  the key.
+     */
+    public ChangeableHolder(
+            final Object key,
+            final V      value) {
+        this.key = (key == NOKEY) ? null : Optional.ofNullable(key);
+        this.value.set(value);
+    }
+    
+    @Override
+    public V get() {
+        return this.value.get();
+    }
+    
+    /**
+     * Change the value without using key.
+     * 
+     * This is be successful only if the key was not set.
+     * 
+     * @param  value  the value.
+     * @return {@code true} if successful.
+     */
+    public boolean set(
+            final V value) {
+        if (this.key != null) {
+            return false;
+        }
+        
+        this.value.set(value);
+        return true;
+    }
+    
+    /**
+     * Change the value using a key.
+     * 
+     * This will only be successful only if the key was set and equals to the given key.
+     * 
+     * @param  key    the key.
+     * @param  value  the value.
+     * @return {@code} true if success.
+     */
+    public boolean set(
+            final Object key,
+            final V      value) {
+        if (this.key == null) {
+            return false;
+        }
+        if (!Objects.equals(this.key.orElse(null), key)) {
+            return false;
+        }
+        
+        this.value.set(value);
+        return true;
+    }
+    
+    //== Static creation ===============================================================================================
+    
+    /**
+     * Create and return a new {@code ChangeableHolder}.
+     **/
+    public static <T> ChangeableHolder<T> changeable(
+            final Object key,
+            final T      value) {
+        return new ChangeableHolder<T>(key, value);
+    }
+    
+    /**
+     * Create and return a new {@code ChangeableHolder}.
+     **/
+    public static <T> ChangeableHolder<T> of(
+            final Object key,
+            final T      value) {
+        return new ChangeableHolder<T>(key, value);
+    }
+    
+}
---
+++ DirectSupplier/src/test/java/direct/supplier/holder/ChangeableHolderTest.java
@@ -0,0 +1,76 @@
+package direct.supplier.holder;
+
+import static direct.supplier.holder.ChangeableHolder.NOKEY;
+import static direct.supplier.holder.ChangeableHolder.changeable;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import direct.supplier.holder.ChangeableHolder;
+
+public class ChangeableHolderTest {
+    
+    private final String INIT_VALUE = "";
+    
+    private ChangeableHolder<String> holder;
+    
+    private ChangeableHolder<String> createHolder(Object key) {
+        holder = (key != null) ? changeable(key, INIT_VALUE) : changeable(NOKEY, INIT_VALUE);
+        return holder;
+    }
+    
+    @Test
+    public void whenNoKeyAssigned_anyOneCanChange() {
+        holder = createHolder(null);
+        assertEquals(INIT_VALUE, holder.get());
+        
+        holder.set("42");
+        assertEquals("42", holder.get());
+    }
+    
+    @Test
+    public void whenKeyAssigned_keyIsNeededWhenChange() {
+        Object key = new Object();
+        holder = createHolder(key);
+        assertEquals(INIT_VALUE, holder.get());
+        
+        holder.set(key, "42");
+        assertEquals("42", holder.get());
+    }
+    
+    @Test
+    public void whenKeyAssigned_givenNoKeyDoesNotChangeValue() {
+        Object key = new Object();
+        holder = createHolder(key);
+        assertEquals(INIT_VALUE, holder.get());
+        
+        holder.set("42");
+        assertEquals(INIT_VALUE, holder.get());
+    }
+    
+    @Test
+    public void whenKeyAssigned_theKeyEqualsIsUsed() {
+        Object key = new Object() {
+            @Override
+            public boolean equals(Object obj) {
+                return (this == obj);
+            }
+        };
+        Object fakeKey = new Object() {
+            @Override
+            public boolean equals(Object obj) {
+                return true;
+            }
+        };
+        
+        holder = createHolder(key);
+        assertEquals(INIT_VALUE, holder.get());
+        
+        holder.set(key, "42");
+        assertEquals("42", holder.get());
+        
+        holder.set(fakeKey, "24");
+        assertEquals("42", holder.get());
+    }
+    
+}
